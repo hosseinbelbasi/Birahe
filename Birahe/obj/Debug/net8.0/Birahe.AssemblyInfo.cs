52d3014ea3a6acfc9ec45ec267d4f75ba315e59d
Hossein <hossein.b8203@gmail.com>
2025-12-04 19:49:16 +0330

    fnial-comiit

--- Birahe/obj/Debug/net8.0/Birahe.AssemblyInfo.cs
+++ Birahe/obj/Debug/net8.0/Birahe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Birahe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adf7076674991bcf26d1acb68e1d545b9a4c1021")]
 [assembly: System.Reflection.AssemblyProductAttribute("Birahe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Birahe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
